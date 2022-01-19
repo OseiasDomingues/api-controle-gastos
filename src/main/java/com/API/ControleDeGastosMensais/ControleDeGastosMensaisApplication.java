2261866367f914ab7b87253db2b00fc51ad81de0
Oséias Domingues <oseiasdomingues9@gmail.com>
2022-01-19 18:42:08 -0300

    Criação do projeto

---
+++ src/main/java/com/API/ControleDeGastosMensais/ControleDeGastosMensaisApplication.java
@@ -0,0 +1,13 @@
+package com.API.ControleDeGastosMensais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeGastosMensaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeGastosMensaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API/ControleDeGastosMensais/ControleDeGastosMensaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.ControleDeGastosMensais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeGastosMensaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
